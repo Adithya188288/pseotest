6601caa12e033f0e2b5d2b5234f01214627b43c1
adithya <adithya@swipepages.com>
2025-04-17 13:00:19 +0530

    adding basepath and assetsPath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
+    assetPrefix: '/pseotest/',
+    basePath: '/pseotest',
 };
 
 export default nextConfig;
